478614696c063194e50b100108b942e4bc724a12
LuizPaulo <luizhat10@gmail.com>
2023-01-08 12:26:49 -0300

    teste de integracao jpa/repository

--- src/test/java/com/luiz/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/luiz/libraryapi/model/repository/BookRepositoryTest.java
@@ -1,5 +1,62 @@
 package com.luiz.libraryapi.model.repository;
 
-public class BookRepositoryTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.luiz.libraryapi.model.entity.Book;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")  
+@DataJpaTest
+public class BookRepositoryTest {
+	
+	@Autowired
+	TestEntityManager entityManager; 
+	
+	@Autowired
+	BookRepository repository;
+	
+	@Test
+	@DisplayName ("Deve retornar verdadeiro quando existir um livro na base com o isbn informado")
+	public void returnTrueWhenIsbnExists() {
+		//cenario
+		
+		String isbn = "123";
+		Book book = Book.builder().author("Fulano").title("as aventuras").isbn(isbn).build();
+		entityManager.persist(book);
+		
+		//execucao
+		
+		boolean exists = repository.existsByIsbn(isbn);
+		
+		//verificacao
+		assertThat(exists).isTrue();
+		
+	}
+	
+	@Test
+	@DisplayName ("Deve retornar falso quando nao existir um livro na base com o isbn informado")
+	public void returnFalseWhenIsbnDoesntExists() {
+		//cenario
+		
+		String isbn = "123";
+		
+		
+		
+		//execucao
+		
+		boolean exists = repository.existsByIsbn(isbn);
+		
+		//verificacao
+		assertThat(exists).isFalse();
+		
+	}
 }
